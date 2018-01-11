7964309f5d421e1a9a268357686eed02f3be765b
Rob Copeland <copeland.robert@gmail.com>
2018-01-11 10:54:10 -0500

    added a route system to load patterns

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ use Slim\Http\Response;
 $app->get('/[{name}]', function (Request $request, Response $response, array $args) {
     $subtemplate = "";
     if ($args['name']) {
-      $subtemplate = 'patterns/' . $args['name'] . '.twig';
+      $subtemplate = 'patterns/' . filter_var($args['name'], FILTER_SANITIZE_STRING) . '.twig';
     }
 
     return $this->view->render($response, 'default.twig', [
